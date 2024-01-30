69d59ab8ede065fa4b6d457c368417b325810396
Vatchala <vatchu@MacBook-Pro-4.local>
2024-01-30 23:51:21 +0530

    im tired

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const targetWords = ['Thank', 'you', 'for','being','mine','philtatos']; // Replace with your words for each day
 const clues = ['Be grateful', 'The man with the hottest body ever', '4','human-_____','you are?','my beloved']; // Replace with your clues for each day
 const loveLetterImages = [
-  'loveletter1.png',
-  'loveletter1.png',
-  'loveletter1.png'
+  'path/to/love_letter_1.png',
+  'path/to/love_letter_2.png',
+  'path/to/love_letter_3.png'
 ]; // Replace with the paths to your love letter images for each day
 
 let currentDay = 0;
@@ -22,11 +22,12 @@ function submitForm() {
   const inputWord = document.getElementById('word').value.toUpperCase();
   
   if (inputWord === targetWords[currentDay]) {
+    showLetter();
     currentDay++;
-    if (currentDay < clues.length) {
-      showClue();
+    if (currentDay < targetWords.length) {
+      alert(`Congratulations! You revealed letter ${currentDay + 1}`);
+      showClue(); // Show the clue for the next day
     } else {
-      showLetter();
       alert('All letters revealed!');
     }
   } else {
@@ -35,4 +36,6 @@ function submitForm() {
 }
 
 document.addEventListener('DOMContentLoaded', showClue);
+
+
 
