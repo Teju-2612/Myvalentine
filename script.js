c6aae3a1530fd10ab962b57f1e1dbfd3b6ed9f46
Vatchala <vatchu@MacBook-Pro-4.local>
2024-01-30 23:39:47 +0530

    Update love letter images in script.js

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-const words = ['LOVE', 'IS', 'IN', 'THE', 'AIR']; // Replace with your words for each day
+const words = ['thank','you','for','being','mine','philtatos']; // Replace with your words for each day
+const loveLetterImages = [
+  'loveletter1.png',
+  'loveletter1.png',
+  'loveletter1.png',
+  'loveletter1.png',
+  'loveletter1.png',
+  'loveletter1.png',
+  'loveletter1.png',
+
+]; // Replace with your image paths
 let currentDay = 0;
 
 function showLetter() {
     if (currentDay < words.length) {
-        const letterContainer = document.getElementById('letter-container');
-        letterContainer.innerHTML = words[currentDay];
+        const loveLetterImage = document.getElementById('love-letter-image');
+        loveLetterImage.src = loveLetterImages[currentDay];
+        loveLetterImage.style.display = 'block'; // Display the love letter image
     } else {
-        alert('All letters revealed!');
+        alert('All love letters revealed!');
     }
 }
 
 function submitForm() {
     const inputWord = document.getElementById('word').value;
     const targetWord = words[currentDay];
-    
+
     if (inputWord.toUpperCase() === targetWord) {
         currentDay++;
         showLetter();
