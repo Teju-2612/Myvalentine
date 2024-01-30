d3dcd405f036e084b1eb9b65b163022ed056dc6d
Vatchala <vatchu@MacBook-Pro-4.local>
2024-01-30 23:45:20 +0530

    New js

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
-const words = ['thank','you','for','being','mine','philtatos']; // Replace with your words for each day
+const targetWords = ['Thank', 'you', 'for','being','mine','philtatos']; // Replace with your words for each day
+const clues = ['Be grateful', 'The man with the hottest body ever', '4','human-_____','you are?','my beloved']; // Replace with your clues for each day
 const loveLetterImages = [
   'loveletter1.png',
   'loveletter1.png',
-  'loveletter1.png',
-  'loveletter1.png',
-  'loveletter1.png',
-  'loveletter1.png',
-  'loveletter1.png',
+  'loveletter1.png'
+]; // Replace with the paths to your love letter images for each day
 
-]; // Replace with your image paths
 let currentDay = 0;
 
+function showClue() {
+  const letterContainer = document.getElementById('letter-container');
+  letterContainer.innerHTML = clues[currentDay];
+}
+
 function showLetter() {
-    if (currentDay < words.length) {
-        const loveLetterImage = document.getElementById('love-letter-image');
-        loveLetterImage.src = loveLetterImages[currentDay];
-        loveLetterImage.style.display = 'block'; // Display the love letter image
-    } else {
-        alert('All love letters revealed!');
-    }
+  const letterContainer = document.getElementById('letter-container');
+  letterContainer.innerHTML = `<img src="${loveLetterImages[currentDay]}" alt="Love Letter">`;
 }
 
 function submitForm() {
-    const inputWord = document.getElementById('word').value;
-    const targetWord = words[currentDay];
-
-    if (inputWord.toUpperCase() === targetWord) {
-        currentDay++;
-        showLetter();
+  const inputWord = document.getElementById('word').value.toUpperCase();
+  
+  if (inputWord === targetWords[currentDay]) {
+    currentDay++;
+    if (currentDay < clues.length) {
+      showClue();
     } else {
-        alert('Incorrect word. Try again!');
+      showLetter();
+      alert('All letters revealed!');
     }
+  } else {
+    alert('Incorrect word. Try again!');
+  }
 }
 
-document.addEventListener('DOMContentLoaded', showLetter);
+document.addEventListener('DOMContentLoaded', showClue);
+
